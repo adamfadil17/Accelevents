100d3b5461f89fd4f2e6df58155c3e591e7328e3
Adam Fadilah Islamawan <adam.fadilah17@gmail.com>
2024-07-21 16:29:52 +0700

    connectdbfix

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -26,14 +26,15 @@ import mongoose from 'mongoose';
 let isConnected = false;
 
 export const connectToDB = async () => {
-    mongoose.set('strictQuery', true);
+  mongoose.set('strictQuery', true);
 
-    if (!process.env.MONGODB_URL) return console.log('MONGODB_URL not found');
-    if (isConnected) return console.log('Already connected to MongoDB');
+  if (!process.env.MONGODB_URL) return console.log('MONGODB_URL not found');
+  if (isConnected) return console.log('Already connected to MongoDB');
 
-    try {
-        await mongoose.connect(process.env.MONGODB_URL);
-        isConnected = true;
-    } catch (error) {
-        console.log(error)
-    }
+  try {
+    await mongoose.connect(process.env.MONGODB_URL);
+    isConnected = true;
+  } catch (error) {
+    console.log(error);
+  }
+};
