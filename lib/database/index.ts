11f08404baa5fd7e70133bf528cc71bccc37db29
Adam Fadilah Islamawan <adam.fadilah17@gmail.com>
2024-07-21 16:37:45 +0700

    connectdbfix3

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,40 +1,40 @@
-// import mongoose from 'mongoose';
-
-// const MONGODB_URI = process.env.MONGODB_URI;
-
-// let cached = (global as any).mongoose || { conn: null, promise: null };
-
-// export const connectToDatabase = async () => {
-//   if (cached.conn) return cached.conn;
-
-//   if (!MONGODB_URI) throw new Error('MONGODB_URI is missing');
-
-//   cached.promise =
-//     cached.promise ||
-//     mongoose.connect(MONGODB_URI, {
-//       dbName: 'accelevents',
-//       bufferCommands: false,
-//     });
-
-//   cached.conn = await cached.promise;
-
-//   return cached.conn;
-// };
-
 import mongoose from 'mongoose';
 
-let isConnected = false;
+const MONGODB_URI = process.env.MONGODB_URI;
+
+let cached = (global as any).mongoose || { conn: null, promise: null };
 
 export const connectToDatabase = async () => {
-  mongoose.set('strictQuery', true);
+  if (cached.conn) return cached.conn;
 
-  if (!process.env.MONGODB_URI) return console.log('MONGODB_URL not found');
-  if (isConnected) return console.log('Already connected to MongoDB');
+  if (!MONGODB_URI) throw new Error('MONGODB_URI is missing');
 
-  try {
-    await mongoose.connect(process.env.MONGODB_URI);
-    isConnected = true;
-  } catch (error) {
-    console.log(error);
-  }
+  cached.promise =
+    cached.promise ||
+    mongoose.connect(MONGODB_URI, {
+      dbName: 'accelevents',
+      bufferCommands: false,
+    });
+
+  cached.conn = await cached.promise;
+
+  return cached.conn;
 };
+
+// import mongoose from 'mongoose';
+
+// let isConnected = false;
+
+// export const connectToDatabase = async () => {
+//   mongoose.set('strictQuery', true);
+
+//   if (!process.env.MONGODB_URI) return console.log('MONGODB_URL not found');
+//   if (isConnected) return console.log('Already connected to MongoDB');
+
+//   try {
+//     await mongoose.connect(process.env.MONGODB_URI);
+//     isConnected = true;
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
