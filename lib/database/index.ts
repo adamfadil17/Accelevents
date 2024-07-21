c8093304a8220c0930cb3d601920e75967f797b5
Adam Fadilah Islamawan <adam.fadilah17@gmail.com>
2024-07-21 16:31:49 +0700

    correctdbfix2

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -25,7 +25,7 @@ import mongoose from 'mongoose';
 
 let isConnected = false;
 
-export const connectToDB = async () => {
+export const connectToDatabase = async () => {
   mongoose.set('strictQuery', true);
 
   if (!process.env.MONGODB_URI) return console.log('MONGODB_URL not found');
