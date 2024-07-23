e817779d29ea2ea6e477ca79c42b323d714c7a31
Adam Fadilah Islamawan <adam.fadilah17@gmail.com>
2024-07-23 10:08:23 +0700

    any type

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -4,9 +4,9 @@ const UserSchema = new mongoose.Schema({
   clerkId: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   username: { type: String, required: true, unique: true },
+  photo: { type: String, required: true },
   firstname: { type: String },
   lastname: { type: String },
-  photo: { type: String, required: true },
 });
 
 const User = mongoose.models.User || mongoose.model('User', UserSchema);
