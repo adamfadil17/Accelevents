c09641930e2b00d38ab7a6c97917aa2215108052
Adam Fadilah Islamawan <adam.fadilah17@gmail.com>
2024-07-23 19:20:32 +0700

    fix6

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -1,14 +1,31 @@
-import mongoose from 'mongoose';
+import { Schema, model, models } from "mongoose";
 
-const UserSchema = new mongoose.Schema({
-  clerkId: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
-  username: { type: String, required: true, unique: true },
-  photo: { type: String, required: true },
-  firstname: { type: String },
-  lastname: { type: String },
+const UserSchema = new Schema({
+  clerkId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  photo: {
+    type: String,
+    required: true,
+  },
+  firstName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+  },
 });
 
-const User = mongoose.models.User || mongoose.model('User', UserSchema);
+const User = models?.User || model("User", UserSchema);
 
 export default User;
